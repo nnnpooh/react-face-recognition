9003351d73d67992a07d8dfca1fd8eb7e9939070
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-01-28 21:13:31 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -7,68 +7,68 @@ import Metatags from "../components/Metatags";
 let ml5;
 
 //Teachable machine url after uploading your model
-const modelURL = "https://teachablemachine.withgoogle.com/models/o67qKOlz-/";
+const modelURL = "https://teachablemachine.withgoogle.com/models/t7fdAKtyH/";
 let classifier;
 
 export default function Home() {
-	const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-	//React hook for accessing the DOM element
-	const webcamRef = useRef(null);
+  //React hook for accessing the DOM element
+  const webcamRef = useRef(null);
 
-	//Label to be predicted by our classifier
-	const [label, setLabel] = useState("");
+  //Label to be predicted by our classifier
+  const [label, setLabel] = useState("");
 
-	const videoConstraints = {
-		width: 1280,
-		height: 720,
-		facingMode: "user",
-	};
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user",
+  };
 
-	//We're using asynchronous useEffect so we can wait for our classifier to be initialized before we attempt to use it
-	useEffect(() => {
-		ml5 = require("ml5"); //Workaround because of "window is not defined" error
-		(async () => {
-			classifier = await ml5.imageClassifier(modelURL + "model.json");
-			classifyVideo();
-		})();
-	}, []);
+  //We're using asynchronous useEffect so we can wait for our classifier to be initialized before we attempt to use it
+  useEffect(() => {
+    ml5 = require("ml5"); //Workaround because of "window is not defined" error
+    (async () => {
+      classifier = await ml5.imageClassifier(modelURL + "model.json");
+      classifyVideo();
+    })();
+  }, []);
 
-	const classifyVideo = () => {
-		try {
-			loading && setLoading(false);
-			//Get the classifications and pass it to callback function
-			classifier.classify(webcamRef.current.video, gotResults);
-		} catch (err) {
-			console.log(err.message);
-		}
-	};
+  const classifyVideo = () => {
+    try {
+      loading && setLoading(false);
+      //Get the classifications and pass it to callback function
+      classifier.classify(webcamRef.current.video, gotResults);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
-	const gotResults = async (error, results) => {
-		const label = results[0].label; //Predicted label with highest confidence
-		setLabel(label);
-		classifyVideo(); // Run on next webcam image
-	};
+  const gotResults = async (error, results) => {
+    const label = results[0].label; //Predicted label with highest confidence
+    setLabel(label);
+    classifyVideo(); // Run on next webcam image
+  };
 
-	return (
-		<div className={styles.main}>
-			<Metatags />
-			{loading ? (
-				<CircularProgress style={{ marginTop: 200 }} />
-			) : (
-				<>
-					<b style={{ fontSize: 50 }}>{"Username: " + label}</b>
+  return (
+    <div className={styles.main}>
+      <Metatags />
+      {loading ? (
+        <CircularProgress style={{ marginTop: 200 }} />
+      ) : (
+        <>
+          <b style={{ fontSize: 50 }}>{"Username: " + label}</b>
 
-					<WebCam
-						audio={false}
-						height={480}
-						ref={webcamRef}
-						screenshotFormat="image/jpeg"
-						width={720}
-						videoConstraints={videoConstraints}
-					/>
-				</>
-			)}
-		</div>
-	);
+          <WebCam
+            audio={false}
+            height={480}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={720}
+            videoConstraints={videoConstraints}
+          />
+        </>
+      )}
+    </div>
+  );
 }
